17c6169e8fa249a3378f4c1e6db7d49b8b02cd03
EthanJ-Brady <github@ethanbrady.xyz>
2024-02-26 17:52:14 -0700

    Switched to lazy.nvim as my plugin manager

--- nvim/.config/nvim/init.vim
+++ nvim/.config/nvim/init.vim
@@ -1,25 +1,6 @@
-call plug#begin('~/.config/nvim/plugged')
-    Plug 'ryanoasis/vim-devicons' "NerdTree icons
-    Plug 'scrooloose/nerdtree' "File explorer
-    Plug 'preservim/nerdcommenter' "Commenting
-    Plug 'github/copilot.vim' "GitHub Copilot
-    Plug 'ap/vim-css-color' "CSS color preview
-    Plug 'terryma/vim-smooth-scroll' "Smooth scroll with ctrl+u and ctrl+d
-    Plug 'tpope/vim-fugitive' "Git commands
-    Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'} "Syntax highlighting
-    Plug 'lambdalisue/glyph-palette.vim' "NerdTree icon colors
-    Plug 'williamboman/mason.nvim' "Package manager
-    Plug 'williamboman/mason-lspconfig.nvim' "LSP
-    Plug 'neovim/nvim-lspconfig' "LSP
-    Plug 'lukas-reineke/indent-blankline.nvim' "Indent line characters
-    Plug 'nvim-lualine/lualine.nvim' "Status line
-    Plug 'lervag/vimtex'
-    Plug 'nvimdev/lspsaga.nvim' "LSP UI
-    Plug 'airblade/vim-gitgutter' "Git diff
-call plug#end()
-
-lua require("plugins")
+lua require("init")
 lua require("colors")
 lua require("keybinds")
+lua require("plugins")
 lua require("options")
 lua require("files")
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,3 +1,60 @@
+-- Setup lazy nvim plugins
+require("lazy").setup({
+    {
+        'ryanoasis/vim-devicons'
+    },                                          -- NerdTree icons
+    {
+        'preservim/nerdtree',
+    },                                          -- File explorer
+    {
+        'preservim/nerdcommenter',
+    },                                          -- Commenting
+    {
+        'github/copilot.vim',
+    },                                          -- GitHub Copilot
+    {
+        'ap/vim-css-color'
+    },                                          -- CSS color preview
+    {
+        'terryma/vim-smooth-scroll'
+    },                                          -- Smooth scroll with ctrl+u and ctrl+d
+    {
+        'tpope/vim-fugitive',
+    },                                          -- Git commands
+    {
+        'nvim-treesitter/nvim-treesitter',
+    },                                          -- Syntax highlighting
+    {
+        'lambdalisue/glyph-palette.vim',
+    },                                          -- NerdTree icon colors
+    {
+        'williamboman/mason.nvim'
+    },                                          -- LSP manager
+    {
+        'williamboman/mason-lspconfig.nvim'
+    },                                          -- LSP
+    {
+        'neovim/nvim-lspconfig'
+    },                                          -- LSP
+    {
+        'lukas-reineke/indent-blankline.nvim'
+    },                                          -- Indent line characters
+    {
+        'nvim-lualine/lualine.nvim'
+    },                                          -- Status line
+    {
+        'lervag/vimtex'
+    },                                          -- Latex helper
+    {
+        'nvimdev/lspsaga.nvim'
+    },                                          -- LSP UI
+    {
+        'airblade/vim-gitgutter',
+    },                                          -- Git diff
+    {
+        'dstein64/vim-startuptime'
+    },                                          -- View startup timing information
+})
 
 -- Setup Mason
 require("mason").setup()
